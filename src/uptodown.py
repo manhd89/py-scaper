a4648000246dc11924501d2cf2e7fcf72ef6d1ff
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-11-19 22:39:14 +0700

    Update uptodown.py

--- src/uptodown.py
+++ src/uptodown.py
@@ -42,7 +42,7 @@ def get_download_link(version: str, app_name: str) -> str:
     content_size = len(response.content)
     logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     soup = BeautifulSoup(response.content, "html.parser")
-    divs = soup.find_all("detail-app-name")
+    divs = soup.find("detail-app-name")
     logging.info(f"{divs}")
     exit(0)
 
