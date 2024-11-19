7140ecaf3f8ac9d022604813e050c601285f2a7d
manhd89 <150607244+manhd89@users.noreply.github.com>
2024-11-19 22:32:06 +0700

    Code cũ

--- src/uptodown.py
+++ src/uptodown.py
@@ -1,154 +1,90 @@
 import json
 import logging
-import os
-import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
+import cloudscraper 
 from bs4 import BeautifulSoup
 
 # Configuration
+scraper = cloudscraper.create_scraper()
+scraper.headers.update(
+    {'User-Agent': 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0'}
+)
 logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+  level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-# Create Chrome driver with headless options
-def create_chrome_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run in headless mode
-    chrome_options.add_argument("--no-sandbox")  # Bypass sandbox mode
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-    chrome_options.add_argument("start-maximized")  # Maximize window
-    chrome_options.add_argument("disable-infobars")  # Disable infobars
-    chrome_options.add_argument("--disable-extensions")  # Disable extensions
-    chrome_options.add_argument(
-        f"user-agent=Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0"
-    )
-
-    driver = webdriver.Chrome(options=chrome_options)
-    return driver
-
-# Click on 'See more' button if necessary
-def click_see_more(driver):
-    try:
-        see_more_button = driver.find_element(By.ID, "button-list-more")
-        if see_more_button:
-            logging.info("Clicking 'See more' to load more versions.")
-            see_more_button.click()
-    except NoSuchElementException:
-        logging.info("No 'See more' button found, all versions are already loaded.")
-        pass
-
-# Get the latest version of the app
 def get_latest_version(app_name: str) -> str:
     conf_file_path = f'./apps/uptodown/{app_name}.json'
-
     with open(conf_file_path, 'r') as json_file:
         config = json.load(json_file)
 
     url = f"https://{config['name']}.en.uptodown.com/android/versions"
-    
-    driver = create_chrome_driver()  # Create driver
-    driver.get(url)
-    
-    soup = BeautifulSoup(driver.page_source, "html.parser")  # Parse HTML from Selenium
-    driver.quit()
 
+    response = scraper.get(url)
+    response.raise_for_status()
+    content_size = len(response.content)
+    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
+    soup = BeautifulSoup(response.content, "html.parser")
     version_spans = soup.select('#versions-items-list .version')
-    
-    versions = [span.text.strip() for span in version_spans]
+    versions = [span.text for span in version_spans]
     highest_version = max(versions)
-    
-    logging.info(f"Highest version found for {app_name}: {highest_version}")
+    logging.info(f"{highest_version}")    
     return highest_version
 
-# Get download link for a specific version
 def get_download_link(version: str, app_name: str) -> str:
     conf_file_path = f'./apps/uptodown/{app_name}.json'
-
     with open(conf_file_path, 'r') as json_file:
         config = json.load(json_file)
-    
+  
     url = f"https://{config['name']}.en.uptodown.com/android/versions"
 
-    driver = create_chrome_driver()  # Create driver
-    driver.get(url)
+    response = scraper.get(url)
+    response.raise_for_status()
+    content_size = len(response.content)
+    logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
+    soup = BeautifulSoup(response.content, "html.parser")
+    divs = soup.find_all("div", {"data-url": True})
+
+    for div in divs:
+        version_span = div.find("span", class_="version")
+        if version_span and version_span.text == version:
+            dl_url = div["data-url"]
+            response = scraper.get(dl_url)
+            response.raise_for_status()
+            content_size = len(response.content)
+            logging.info(f"URL:{response.url} [{content_size}/{content_size}] -> \"-\" [1]")
     
-    soup = BeautifulSoup(driver.page_source, "html.parser")
+            soup = BeautifulSoup(response.content, "html.parser")
+            button = soup.find('button', id='detail-download-button')
+            data_url = button['data-url']
+            if data_url:
+                full_url = "https://dw.uptodown.com/dwn/" + data_url
+                return full_url
 
-    while True:
-        divs = soup.find_all("div", {"data-url": True})
-        for div in divs:
-            version_span = div.find("span", class_="version")
-            if version_span and version_span.text.strip() == version:
-                dl_url = div["data-url"]
-                
-                # Navigate to the version-specific download page
-                logging.info(f"Found download page for version {version}, navigating to it...")
-                driver.get(dl_url)
-
-                # Parse the download page for the actual download link
-                soup = BeautifulSoup(driver.page_source, "html.parser")
-                download_button = soup.find('button', {'id': 'detail-download-button'})
-                if download_button and download_button.get('data-url'):
-                    data_url = download_button.get('data-url')
-                    full_url = f"https://dw.uptodown.com/dwn/{data_url}"
-                    logging.info(f"Found download link: {full_url}")
-
-                    # Get the final download URL from the server response
-                    response = requests.get(full_url)
-                    if response.status_code == 200:
-                        download_url = response.url  # Get the URL from the response
-                        logging.info(f"Final download URL: {download_url}")
-                        driver.quit()
-                        return download_url
-
-        # If the "See more" button is available, click to load more versions
-        click_see_more(driver)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-
-    driver.quit()
     return None
 
-# Download APK or XAPK resource from URL using requests
 def download_resource(url: str, name: str) -> str:
-    if not url:
-        logging.error(f"Download URL is None. Cannot download {name}.")
-        return None
+    filepath = f"./{name}"
 
-    # Use requests to get the final URL after redirections
-    response = requests.get(url, allow_redirects=True)
-    final_url = response.url
-    logging.info(f"Final URL after redirections: {final_url}")
+    with scraper.get(url, stream=True) as res:
+        res.raise_for_status()
 
-    # Extract file extension from the final URL
-    file_extension = os.path.splitext(final_url)[1]  # This will return .apk, .xapk, etc.
+        final_url = res.url  # Get the final URL after any redirects
+        total_size = int(res.headers.get('content-length', 0))
+        downloaded_size = 0
 
-    # Dynamically adjust the filename with the correct extension
-    filepath = f"./{name}{file_extension}"
+        with open(filepath, "wb") as file:
+            for chunk in res.iter_content(chunk_size=8192):
+                file.write(chunk)
+                downloaded_size += len(chunk)
+                
+        logging.info(
+            f"URL:{final_url} [{downloaded_size}/{total_size}] -> \"{name}\" [1]"
+        )
 
-    # Now download the file using requests
-    logging.info(f"Downloading the file from {final_url} to {filepath}")
-
-    # Stream the download to avoid memory overload with large files
-    with requests.get(final_url, stream=True) as r:
-        r.raise_for_status()
-        with open(filepath, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                f.write(chunk)
-
-    logging.info(f"Downloaded {name} to {filepath}")
     return filepath
 
-# Main function to download app from Uptodown
 def download_uptodown(app_name: str) -> str:
     version = get_latest_version(app_name)
     download_link = get_download_link(version, app_name)
-    filename = f"{app_name}-v{version}"
-    
-    if download_link:
-        return download_resource(download_link, filename)
-    else:
-        logging.error("Failed to retrieve the download link.")
-        return None
+    filename = f"{app_name}-v{version}.apk"
+    return download_resource(download_link, filename)
